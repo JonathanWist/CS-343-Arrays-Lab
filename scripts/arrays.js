5011df800af683984d3dd1a6e2279ba405d8f29e
Jonathan Wist <jpwist@live.com>
2022-10-24 14:22:55 -0400

    Updated arrays.js so that it can properly filter results and update the summary data with the currently displayed information

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -20,14 +20,71 @@ const courseToCard = ({prefix, number, title, url, desc, prereqs, credits}) => {
   return courseTemplate;
 };
 
+const resultsContainer = document.querySelector("#filtered");
+const summaryContainer = document.querySelector("#summary");
 const courseCards = data.items.map(courseToCard);
-courseCards.forEach((c) => document.write(c));
+resultsContainer.innerHTML = courseCards.join("");
 //document.write(courseCards.join(''));
+
+// 3. we update the result count and related summary info as we filter
+
+const updateSummary = (cards) => {
+  let items = cards.length;
+  console.log(cards);
+  console.log(items);
+
+  const initial = 0;
+  const sumOfCreditHours = cards.reduce((prev, curr) => {
+    return prev + curr.credits;
+  }, initial);
+  console.log(sumOfCreditHours);
+
+  const uniquePrereqs = new Set();
+  const sumOfPrereqHours = cards.reduce((prev, curr) => {
+    let courseCredits = 0;
+    curr.prereqs.forEach((prereq) => {
+      if (!uniquePrereqs.has(prereq)) {
+        uniquePrereqs.add(prereq);
+        const course = data.items.find((course) => course.number == prereq);
+        courseCredits = course.credits;
+        /*courseCredits = data.items.reduce((prevcard, card) => {
+          let credits = 0;
+          if (card.number == prereq) {
+            credits = card.credits;
+          }
+          return prevcard + credits;}, initial);*/
+      }
+    })
+    return prev + courseCredits;
+  }, initial);
+  
+  const summaryTemplate = `<h2>Summary</h2>
+  <dl>
+    <dt>Count</dt>
+    <dd><span>${items}</span> items</dd>
+    <dt>Cost</dt>
+    <dd><span>${sumOfCreditHours}</span> credit-hours + <span>${sumOfPrereqHours}</span> credit-hours of prereqs</dd>
+  </dl>`
+  return summaryTemplate;
+};
+
+window.onload = (ev) => {summaryContainer.innerHTML = updateSummary(data.items)};
 
 // 2. maybe we only show those that match the search query?
 
 const filterCourseCard = (markup, query) => {
   return markup.toLowerCase().includes(query.toLowerCase());
+};
+
+const filterData = (card, query) => {
+  if (query == card.number || query == card.credits) {
+    return true;
+  } else if (filterCourseCard(card.title, query) || filterCourseCard(card.prefix, query) 
+  || filterCourseCard(card.desc, query)) {
+    return true;
+  } else {
+    return card.prereqs.some(prereq => prereq == query);
+  }
 }
 
 const searchButton = document.getElementById("search-btn");
@@ -35,11 +92,10 @@ searchButton.addEventListener("click", (ev) => {
   ev.preventDefault();
   const searchField = document.querySelector('input[name="query-text"]');
   const queryText = searchField.value;
-  const filtered = courseCards.filter((card) => filterCourseCard(card, queryText));
-  console.log(filtered);
-  filtered.forEach((card) => {
-    document.write(card);
-  });
-})
-
-// 3. we update the result count and related summary info as we filter
+  const filteredData = data.items.filter((card) => filterData(card, queryText));
+  console.log(filteredData);
+  //const filteredCards = courseCards.filter((card) => filterCourseCard(card, queryText));
+  const filteredCards = filteredData.map(courseToCard);
+  resultsContainer.innerHTML = filteredCards.join("");
+  summaryContainer.innerHTML = updateSummary(filteredData);
+});
